d03ae6b5046d486c3b695de40b1e468ae14dc9e4
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-04-14 17:37:43 -0500

    Created user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to dashboard_path(user)
+		else
+			render :new
+		end
 	end
 
 	def show
@@ -13,6 +21,15 @@ class UsersController < ApplicationController
 	end
 
 	def update
+	end
+
+	def dashboard
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#welcome'
+  get '/users/:id/dashboard', to: 'users#dashboard', as: :dashboard
   resources :users, only: [:new, :create, :show, :edit, :update]
 end
