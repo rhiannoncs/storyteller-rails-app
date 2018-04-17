d896fe61e6264bced2f2ef73d264e7db189603a1
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-04-16 19:58:16 -0500

    Added current_user and user verification methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	before_action :verify_user
+	helper_method :current_user
 
 	def welcome
+	end
+
+	private
+
+	def current_user
+		User.find(session[:user_id])
+	end
+
+	def authenticated_user?
+		!!current_user
+	end
+
+	def verify_user
+		redirect_to root_path unless authenticated_user?
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+	skip_before_action :verify_user, only: [:new, :create, :create_omniauth]
 
 	def new
 	end
