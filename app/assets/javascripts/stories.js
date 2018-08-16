1a2a487f6790bc1fc3054b4eadadb781f1679ade
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-08-16 10:57:23 -0500

    Added story details template to stories.js

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
 	$(".js-story-details").click(function() {
 		var id = $(this).data("id");
 		$.get(`/stories/${id}/details`, function(data) {
-			alert(data["title"]);
+			$(`#detail-box-${id}`).html(details(data));
 		})
 	})
 })
+
+function details(story_data) {
+	let authors = []
+	story_data["contributors"].forEach(function(contributor) {
+		authors.push(contributor.name);
+	})
+	let details = `<strong>Genre:</strong> ${story_data["genre"]}<br>
+	<strong>Description:</strong> ${story_data["description"]}<br><strong>Contributors:</strong> ${authors}`;
+	return details
+}
--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,4 +1,4 @@
 class StorySerializer < ActiveModel::Serializer
   attributes :id, :title, :genre, :description
-  has_many :users, through: :submissions
+  has_many :contributors
 end
