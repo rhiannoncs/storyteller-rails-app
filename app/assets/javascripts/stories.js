5bbb79f6d84437c609d9388fd3202581eb7cf251
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-08-21 10:29:40 -0500

    Added data to next_story JS method

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -19,17 +19,22 @@ function show_details(button) {
 function show_story(link) {
 	var id = $(link).data("id");
 	$.get(`/stories/${id}/details`, function(data) {
-		let story = new Story(data["id"], data["title"], data["genre"], data["description"], data["contributors"], data["submissions"]);
+		let story = new Story(data["id"], data["title"], data["genre"], data["description"], data["team"], data["contributors"], data["submissions"]);
 		$(".story_title").text(story.title);
+		$(".story_team").html(`<h3><a href="/teams/${story.team.id}">By: ${story.team.name}</a></h3>`);
+		$(".story_status").text("Status: ");
+		$(".story_contributors").html(story.contributor_details());
+		$(".story_content").html(story.content());
+
 	})
-	console.log(id);
 }
 
-function Story(id, title, genre, description, contributors, submissions) {
+function Story(id, title, genre, description, team, contributors, submissions) {
 	this.id = id;
 	this.title = title;
 	this.genre = genre;
 	this.description = description;
+	this.team = team;
 	this.contributors = contributors;
 	this.submissions = submissions;
 }
@@ -42,4 +47,20 @@ Story.prototype.details = function() {
 	let details = `<strong>Genre:</strong> ${this.genre}<br><strong>Description:</strong> ${this.description}<br>
 	<strong>Contributors:</strong> ${authors}`;
 	return details
+}
+
+Story.prototype.contributor_details = function() {
+	let contributor_details = "";
+	this.contributors.forEach(function(contributor) {
+		contributor_details += `<a href="/users/${contributor.id}">${contributor.name}</a><br>`
+	})
+	return contributor_details
+}
+
+Story.prototype.content = function() {
+	let content = "";
+	this.submissions.forEach(function(submission) {
+		content += `<p>${submission.content}</p>`
+	})
+	return content;
 }
--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,5 +1,5 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :title, :genre, :description
+  attributes :id, :title, :genre, :description, :team
   has_many :contributors
   has_many :submissions
 end
